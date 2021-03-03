b7a51e3145adf7f9a28787725c4634f4052fbd81
Carla Holcomb <carla.programmer@gmail.com>
2021-03-03 02:35:14 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
   def display_artist(song)
      song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
    end
- 
+
    # Display dropdown of artists, with name if editing through nested
    def artist_select(song, path)
      if song.artist && path == "nested"
@@ -11,7 +11,7 @@ module ArtistsHelper
        select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
      end
    end
- 
+
    # Display name if editing through nested
    def display_name(song, path)
      if song.artist && path == "nested"
