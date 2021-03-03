d88bbfe2b41d72e1dc8a95d5e1fd9c51ea002751
Carla Holcomb <carla.programmer@gmail.com>
2021-03-03 02:45:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,12 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
-   if params[:artist_id] && !Artist.exists?(params[:artist_id])
-     redirect_to artists_path, alert: "Artist not found"
-   else
-     @song = Song.new(artist_id: params[:artist_id])
-   end
- end
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
+  end
 
   def create
     @song = Song.new(song_params)
